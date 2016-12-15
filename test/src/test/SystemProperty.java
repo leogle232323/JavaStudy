fc9b19b5c11bd0d9beb89ca46190aeb5b90238e8
xiaoyang <xiaoyang@zbj.com>
2016-12-15 10:39:48 +0800

    test

---
+++ test/src/test/SystemProperty.java
@@ -0,0 +1,16 @@
+package test;
+
+import java.util.Properties;
+
+public class SystemProperty {
+	public static void main(String[] args){
+		//系统属性
+		Properties sysProperty = System.getProperties();
+		String str = sysProperty.toString();
+		String[] strSplit = str.split("\\,");
+		for(int i = 0;i < strSplit.length;i++){
+			System.err.println(strSplit[i]);
+		}
+		
+	}
+}
