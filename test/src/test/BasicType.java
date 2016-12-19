9fd92efc0b2fbe5295aa816f12ec0daa29bc2e1a
Administrator <Administrator@USER-20160523NW>
2016-12-19 23:58:25 +0800

    大整数类型

---
+++ test/src/test/BasicType.java
@@ -0,0 +1,18 @@
+package test;
+
+import java.math.BigInteger;
+
+public class BasicType {
+	public static void main(String[] args){
+		//1.在堆栈中创建对象
+		char c = 'x';
+		//2.在堆中创建对象，Character是基本类型char的包装器类
+		Character ch = new Character(c);
+		Character ch1 = new Character('x');
+		
+		System.out.println(c);
+		System.out.println(ch);
+		System.out.println(ch1);
+		
+	}
+}
