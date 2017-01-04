c5e30725540d3ea403ce54ff9825168cad7a96cc
xiaoyang <xiaoyang@zbj.com>
2017-01-04 18:09:15 +0800

    本地分支，有内容更改，提交到master

--- test/src/test/TestMerge.java
+++ test/src/test/TestMerge.java
@@ -3,6 +3,7 @@ package test;
 public class TestMerge {
 	public static void main(String[] args){
 		System.out.println("测试合并分支冲突");
-		String hah;
+		String hah = "";
+		System.out.println(hah);
 	}
 }
