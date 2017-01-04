16eedf83a46d4ee792c422b07cb8d442d8d8e99f
xiaoyang <xiaoyang@zbj.com>
2017-01-04 17:49:49 +0800

    66

--- test/src/test/TestMerge.java
+++ test/src/test/TestMerge.java
@@ -4,7 +4,7 @@ public class TestMerge {
 	public static String wuwa;
 	public String hehe;
 	public static String pupu;
-	public String huhu;
+	public String bubu;
 	
 	public static void main(String[] args){
 		System.out.println("测试合并分支冲突");
