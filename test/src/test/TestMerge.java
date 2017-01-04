b5aa1ff3a111d07d571a780b7871dfdeb8e1b458
xiaoyang <xiaoyang@zbj.com>
2017-01-04 18:01:06 +0800

    0000

--- test/src/test/TestMerge.java
+++ test/src/test/TestMerge.java
@@ -1,12 +1,6 @@
 package test;
 
 public class TestMerge {
-	public static String wuwa;
-	public String hehe;
-	public static String pupu;
-	public String bubu;
-	
-	
 	public static void main(String[] args){
 		System.out.println("测试合并分支冲突");
 		String hah;
