c2419b584a3756aaeeaa8445a79e041d3a6747a3
xiaoyang <xiaoyang@zbj.com>
2017-01-04 17:53:34 +0800

    121

--- test/src/test/TestMerge.java
+++ test/src/test/TestMerge.java
@@ -1,10 +1,6 @@
 package test;
 
 public class TestMerge {
-	public static String wuwa;
-	public String hehe;
-	public static String pupu;
-	public String huhu;
 	
 	
 	public static void main(String[] args){
