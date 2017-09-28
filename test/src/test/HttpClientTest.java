c033195398e3fb056dcc49e79b4e106f366ba8ef
leogle <leogle@yeah.net>
2017-09-28 17:43:00 +0800

    1

---
+++ test/src/test/HttpClientTest.java
@@ -0,0 +1,41 @@
+package test;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.http.client.ClientProtocolException;
+import org.junit.Test;
+
+import tools.HttpClientUtil;
+
+public class HttpClientTest {
+	@Test
+	public void testGet() throws ClientProtocolException, IOException {
+		String url = "http://m.zbj.com/user/getUserInfoCenterTask?page=1&size=10&product=3&status=0";
+		System.out.println(HttpClientUtil.sendGet(url));
+	}
+
+	@Test
+	public void testGetWithHeader() throws ClientProtocolException, IOException {
+		String url = "http://m.zbj.com/user/getUserInfoCenterTask?page=1&size=10&product=3&status=0";
+		Map<String, String> headers = new HashMap<>();
+		headers.put("Cookie",
+				"userkey=kuxmCunleO2Mm6mXFiVkXPAoj5lCYIw6yiDIzT9fMA06TV6kv3zXQz5yarnEpmqhEsWwB1Jn8RA%2BMopc1F4URN%2BNU%2FOSs04yV1EV%2Fl2LPcLG%2BVppgubAB4p4nIFITxmmRWy040zb8iT%2Ff2rVdtiJes58VaqTfPAdQT%2BQwJgU1yJxNOYsADOu6k3c4%2Fn1xboIwwrGjy1cgkSUx691PX9hOsF9vDu%2Bgz5%2BDxEXvRsgEWCLe6D71tx6BC1X%2F3Yj9ls%2BXaqnJ8z6QFeRkauOW1tJCPWSYnAXW4iJMDRoosLrI3DlXMTKNqrQqMD1A1z5UvOb3TMcEawspp%2BArJZs");
+		System.out.println(HttpClientUtil.sendGet(url, headers));
+	}
+
+	@Test
+	public void testPost() throws ClientProtocolException, IOException {
+		String url = "http://buyer.zbj.com/service/search/v2";
+		Map<String, Object> json = new HashMap<>();
+		json.put("dk",
+				"eyJpbWVpIjoiMjNCRTcwNjItQjIxMS00NTJFLTlCRTUtRUU1RkIzNkNBQTQ0IiwicGhvbmVfb3NfdmVyc2lvbiI6IjExLjAiLCJjbGllbnRfdHlwZSI6IjEiLCJjbGllbnRfdmVyc2lvbiI6IjUuNC4yIiwic29mdF9mcm9tIjoiYXBwbGUuY29tIiwiZGV2aWNlX2tleSI6IihudWxsKSIsInBob25lX3R5cGUiOiIyIiwiYnVuZGxlX3ZlcnNpb24iOiI1LjQuMiJ9");
+		json.put("keyword", "logo设计");
+		json.put("page", "0");
+		json.put("pagesize", "10");
+
+		System.out.println(HttpClientUtil.sendPost(url, json));
+
+	}
+}
--- test/src/tools/HttpClientUtil.java
+++ test/src/tools/HttpClientUtil.java
@@ -2,7 +2,6 @@ package tools;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
-import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.http.HttpEntity;
@@ -16,7 +15,6 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.json.simple.JSONObject;
-import org.junit.Test;
 
 public class HttpClientUtil {
 	private static CloseableHttpClient client = HttpClients.createDefault();
@@ -37,6 +35,7 @@ public class HttpClientUtil {
 	/**
 	 * 发送get请求，无header
 	 * 
+	 * @param url
 	 * @throws IOException
 	 * @throws ClientProtocolException
 	 */
@@ -48,6 +47,8 @@ public class HttpClientUtil {
 	/**
 	 * 发送get请求，有header
 	 * 
+	 * @param url
+	 * @param headers
 	 * @throws IOException
 	 * @throws ClientProtocolException
 	 * 
@@ -61,48 +62,23 @@ public class HttpClientUtil {
 	}
 
 	/**
-	 * 发送post请求
+	 * 发送post请求，json提交
 	 * 
+	 * @param url
+	 * @param body
 	 * @throws IOException
 	 * @throws ClientProtocolException
 	 * 
 	 */
-	public static String sendPost(String url, JSONObject body) throws ClientProtocolException, IOException {
+	@SuppressWarnings("unchecked")
+	public static String sendPost(String url, Map<String, Object> body) throws ClientProtocolException, IOException {
 		HttpPost httpPost = new HttpPost(url);
-		StringEntity entity = new StringEntity(body.toJSONString(), Charset.forName("UTF-8"));
+		JSONObject json = new JSONObject();
+		json.putAll(body);
+		StringEntity entity = new StringEntity(json.toJSONString(), Charset.forName("UTF-8"));
 		entity.setContentEncoding("UTF-8");
 		entity.setContentType("application/json");
 		httpPost.setEntity(entity);
 		return client.execute(httpPost, responseHandler);
-	}
-
-	@Test
-	public void testGet() throws ClientProtocolException, IOException {
-		String url = "http://m.zbj.com/user/getUserInfoCenterTask?page=1&size=10&product=3&status=0";
-		System.out.println(sendGet(url));
-	}
-
-	@Test
-	public void testGetWithHeader() throws ClientProtocolException, IOException {
-		String url = "http://m.zbj.com/user/getUserInfoCenterTask?page=1&size=10&product=3&status=0";
-		Map<String, String> headers = new HashMap<>();
-		headers.put("Cookie",
-				"userkey=kuxmCunleO2Mm6mXFiVkXPAoj5lCYIw6yiDIzT9fMA06TV6kv3zXQz5yarnEpmqhEsWwB1Jn8RA%2BMopc1F4URN%2BNU%2FOSs04yV1EV%2Fl2LPcLG%2BVppgubAB4p4nIFITxmmRWy040zb8iT%2Ff2rVdtiJes58VaqTfPAdQT%2BQwJgU1yJxNOYsADOu6k3c4%2Fn1xboIwwrGjy1cgkSUx691PX9hOsF9vDu%2Bgz5%2BDxEXvRsgEWCLe6D71tx6BC1X%2F3Yj9ls%2BXaqnJ8z6QFeRkauOW1tJCPWSYnAXW4iJMDRoosLrI3DlXMTKNqrQqMD1A1z5UvOb3TMcEawspp%2BArJZs");
-		System.out.println(sendGet(url, headers));
-	}
-
-	@SuppressWarnings("unchecked")
-	@Test
-	public void testPost() throws ClientProtocolException, IOException {
-		String url = "http://buyer.zbj.com/service/search/v2";
-		JSONObject json = new JSONObject();
-		json.put("dk",
-				"eyJpbWVpIjoiMjNCRTcwNjItQjIxMS00NTJFLTlCRTUtRUU1RkIzNkNBQTQ0IiwicGhvbmVfb3NfdmVyc2lvbiI6IjExLjAiLCJjbGllbnRfdHlwZSI6IjEiLCJjbGllbnRfdmVyc2lvbiI6IjUuNC4yIiwic29mdF9mcm9tIjoiYXBwbGUuY29tIiwiZGV2aWNlX2tleSI6IihudWxsKSIsInBob25lX3R5cGUiOiIyIiwiYnVuZGxlX3ZlcnNpb24iOiI1LjQuMiJ9");
-		json.put("keyword", "logo设计");
-		json.put("page", "0");
-		json.put("pagesize", "10");
-
-		System.out.println(sendPost(url, json));
-
 	}
 }
